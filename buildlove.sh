0fa9ac7958648d4d9773181cffe59f690ed9c8be
ianfitzpatrick1 <ianfitz@gmail.com>
2016-08-17 16:52:00 -0500

    Added build script and basic scrolling with stepping

---
+++ buildlove.sh
@@ -0,0 +1,1 @@
+zip -9 -x research/**\* design/ research/ .DS_Store .git/ .git/**\* .gitignore design/**\* *.md *.sublime-project *.sublime-workspace *.sh -r OurTeam.love .
--- main.lua
+++ main.lua
@@ -2,12 +2,45 @@ io.stdout:setvbuf('no')
 debug = true
 
 function love.load(arg)
+	love.window.setMode( 1280, 720)
+	bg = { x = 0, y = 0 }
+	bg.img = love.graphics.newImage('assets/graphics/backgrounds/football_field_bg.png')
+	love.graphics.setBackgroundColor( 0, 0, 0 )
+	scroll = {rate = 150, step = 800, counter = 0}
 end
 
 function love.update(dt)
-	require('lovebird').update()
+	-- require('lovebird').update()
+	-- Testing screen scrolling
+	if scroll.counter == 0 then
+		canscroll = true
+	else
+		canscroll = false
+	end
+
+	if love.keyboard.isDown('right') then
+		
+		scroll.counter = scroll.counter + scroll.rate
+
+		if bg.x > -4480 and canscroll == true then
+			bg.x = bg.x - scroll.rate
+		end
+	end
+
+	if love.keyboard.isDown('left') then
+		scroll.counter = scroll.counter + scroll.rate
+		if bg.x < 0 and canscroll then
+			bg.x = bg.x + scroll.rate			
+		end
+	end
+
+	if scroll.counter >= scroll.step then
+		scroll.counter = 0
+	end
+
 end
 
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+   love.graphics.draw(bg.img, bg.x, bg.y)
+   love.graphics.print('canscroll ' .. scroll.counter, 50, 50)
 end
