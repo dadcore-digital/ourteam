0fc90561678dd4dfa4163dda46ce846972f66a42
ianfitzpatrick1 <ianfitz@gmail.com>
2016-08-18 01:25:34 -0500

    Kick bar now scrolls screen

--- main.lua
+++ main.lua
@@ -14,7 +14,8 @@ end
 
 function love.load(arg)
 	love.window.setMode( 1280, 720)
-
+	-- love.window.setFullscreen(true, 'exclusive')
+	
 	-- Background Scrolling
 	bg = { x = 0, y = 0 }
 	bg.img = love.graphics.newImage('assets/graphics/backgrounds/football_field_bg.png')
@@ -24,7 +25,7 @@ function love.load(arg)
 
 	-- Football Target
 	target = { x = -4000, reached = true }
-	meter = { strength = 0, max = 250, speed = 5, direction = 'right' }
+	meter = { strength = 0, max = 252, speed = 9, direction = 'right', enabled = true }
 
 end
 
@@ -33,7 +34,7 @@ function love.update(dt)
 
 	canscroll = true and scroll.counter == 0 or false
 
-	-- Auto scroll background until target is reached
+	--! Auto scroll background until target is reached !--
 	if not target.reached then
 
 		scroll.counter = scroll.counter + scroll.rate
@@ -51,23 +52,32 @@ function love.update(dt)
 		scroll.counter = 0
 	end
 
-	-- Flucuate Power Meter
-	if meter.strength < meter.max and 
-		meter.direction == 'right' then
+	--! Flucuate Power Meter !--
+	if meter.enabled then
+		if meter.strength < meter.max and 
+			meter.direction == 'right' then
 
-		meter.strength = meter.strength + 1 * meter.speed
+			meter.strength = meter.strength + 1 * meter.speed
 
-	elseif meter.strength > 0 and 
-		meter.direction == 'left' then
-		
-		meter.strength = meter.strength - 1 * meter.speed
+		elseif meter.strength > 0 and 
+			meter.direction == 'left' then
+			
+			meter.strength = meter.strength - 1 * meter.speed
+		end
+
+		if meter.strength == meter.max then
+			meter.direction = 'left'
+
+		elseif meter.strength == 0 then
+			meter.direction = 'right'
+		end
 	end
 
-	if meter.strength == meter.max then
-		meter.direction = 'left'
-
-	elseif meter.strength == 0 then
-		meter.direction = 'right'
+	--! Stop Power Meter !--
+	if love.keyboard.isDown('space') then
+		meter.enabled = false
+		target.x = (meter.strength * 17) * -1
+		target.reached = false
 	end
 
 
