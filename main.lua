83df687fc13d1f55897f2b50ad56298b7a65587a
ianfitzpatrick1 <ianfitz@gmail.com>
2016-08-17 23:01:15 -0500

    Got basic automatic scrolling working based on target endpoint

--- main.lua
+++ main.lua
@@ -1,36 +1,45 @@
 io.stdout:setvbuf('no')
 debug = true
 
+function debug_print()
+   love.graphics.setColor(0, 0, 0)
+   love.graphics.rectangle('fill', 25, 35, 150, 50)
+
+   love.graphics.setColor(255, 255, 255)
+   love.graphics.print('bg.x: ' .. bg.x,  50, 50)
+end
+
 function love.load(arg)
 	love.window.setMode( 1280, 720)
+	
+	-- Background Scrolling
 	bg = { x = 0, y = 0 }
 	bg.img = love.graphics.newImage('assets/graphics/backgrounds/football_field_bg.png')
 	love.graphics.setBackgroundColor( 0, 0, 0 )
-	scroll = {rate = 150, step = 800, counter = 0}
+	scroll = {rate = 80, step = 500, counter = 0}
+	canscroll = true
+
+	-- Football Target
+	target = { x = -4000, reached = false }
+
 end
 
 function love.update(dt)
-	-- require('lovebird').update()
-	-- Testing screen scrolling
-	if scroll.counter == 0 then
-		canscroll = true
-	else
-		canscroll = false
-	end
+	require('lovebird').update()
 
-	if love.keyboard.isDown('right') then
-		
+	canscroll = true and scroll.counter == 0 or false
+
+	-- Auto scroll background until target is reached
+	if not target.reached then
+
 		scroll.counter = scroll.counter + scroll.rate
 
-		if bg.x > -4480 and canscroll == true then
+		if canscroll == true then
 			bg.x = bg.x - scroll.rate
 		end
-	end
 
-	if love.keyboard.isDown('left') then
-		scroll.counter = scroll.counter + scroll.rate
-		if bg.x < 0 and canscroll then
-			bg.x = bg.x + scroll.rate			
+		if bg.x <= target.x then
+			target.reached = true
 		end
 	end
 
@@ -42,5 +51,11 @@ end
 
 function love.draw()
    love.graphics.draw(bg.img, bg.x, bg.y)
-   love.graphics.print('canscroll ' .. scroll.counter, 50, 50)
+
+   
+   -- Debug output
+   if debug == true then
+   		debug_print()
+   	end
+
 end
